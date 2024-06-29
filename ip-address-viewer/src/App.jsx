b9235fb27d770d35f73ebf47d3bd31e4b869a5fe
Ethan <ejbolts@hotmail.com>
2024-06-30 06:14:24 +1000

    initialised for react project

---
+++ ip-address-viewer/src/App.jsx
@@ -0,0 +1,22 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <p>
+        IP Address Viewer Search for any IP address or domain IP Address
+        Location Timezone UTC
+      </p>
+
+      <div className="attribution">
+        Challenge by{" "}
+        <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+          Frontend Mentor
+        </a>
+        . Coded by <a href="https://www.ethanbolton.com/">Ethan Bolton</a>.
+      </div>
+    </>
+  );
+}
+
+export default App;
